73b177f13cca2a23b2522ba269436aaeea8a50de
Davidlohr Bueso <dave@stgolabs.net>
2024-04-09 21:45:50 -0700

    lala

--- examples/cxl-mctp.c
+++ examples/cxl-mctp.c
@@ -4,29 +4,29 @@
 
 #include <libcxlmi.h>
 
-/* static int show_some_info_from_all_devices(struct cxlmi_ctx *ctx) */
-/* { */
-/* 	int rc = 0; */
-/* 	struct cxlmi_endpoint *ep; */
+static int show_some_info_from_all_devices(struct cxlmi_ctx *ctx)
+{
+	int rc = 0;
+	struct cxlmi_endpoint *ep;
 
-/* 	cxlmi_for_each_endpoint(ctx, ep) { */
-/* 		struct cxlmi_cci_infostat_identify id; */
-/* 		struct cxlmi_cci_get_timestamp ts; */
+	cxlmi_for_each_endpoint(ctx, ep) {
+		struct cxlmi_cci_infostat_identify id;
+		struct cxlmi_cci_get_timestamp ts;
 
-/* 		rc = cxlmi_query_cci_identify(ep, &id); */
-/* 		if (rc) */
-/* 			break; */
-/* 		printf("device type: %s\n", */
-/* 		       id.component_type == 0x03 ? "type3":"switch"); */
+		rc = cxlmi_query_cci_identify(ep, &id);
+		if (rc)
+			break;
+		printf("device type: %s\n",
+		       id.component_type == 0x03 ? "type3":"switch");
 
-/* 		rc = cxlmi_query_cci_timestamp(ep, &ts); */
-/* 		if (rc) */
-/* 			break; */
-/* 		printf("device timestamp: %lu\n", ts.timestamp); */
-/* 	} */
+		rc = cxlmi_query_cci_timestamp(ep, &ts);
+		if (rc)
+			break;
+		printf("device timestamp: %lu\n", ts.timestamp);
+	}
 
-/* 	return rc; */
-/* } */
+	return rc;
+}
 
 int main(int argc, char **argv)
 {
@@ -45,6 +45,8 @@ int main(int argc, char **argv)
 	nid = strtol(argv[1], NULL, 10);
 	eid = strtol(argv[2], NULL, 10);
 
+	printf("ep %d:%d\n", nid, eid);
+
 	if (cxlmi_new_ctx(&ctx)) {
 		fprintf(stderr, "cannot create new context object\n");
 		goto exit;
@@ -56,9 +58,8 @@ int main(int argc, char **argv)
 		goto exit_free_ctx;
 	}
 
-	rc = 0;
 	/* yes, only 1 endpoint, but might add more */
-	/* rc = show_some_info_from_all_devices(ctx); */
+	rc = show_some_info_from_all_devices(ctx);
 
 	cxlmi_close(ep);
 exit_free_ctx:
