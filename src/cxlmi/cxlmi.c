cee5c4c07f135996a4be5aa19c9b5399a3137d71
Davidlohr Bueso <dave@stgolabs.net>
2024-04-19 11:46:39 -0700

    lala

--- src/cxlmi/cxlmi.c
+++ src/cxlmi/cxlmi.c
@@ -529,24 +529,34 @@ CXLMI_EXPORT struct cxlmi_endpoint *cxlmi_next_endpoint(struct cxlmi_ctx *m,
 static int arm_cci_request(struct cxlmi_endpoint *ep, struct cxlmi_cci_msg *req,
 			   size_t req_pl_sz, uint8_t cmdset, uint8_t cmd)
 {
-	if (ep->transport_data) {
-		struct cxlmi_transport_mctp *mctp = ep->transport_data;
+	struct cxlmi_transport_mctp *mctp = ep->transport_data;
 
-		req->category = CXL_MCTP_CATEGORY_REQ;
-		req->tag = mctp->tag++;
-		req->vendor_ext_status = 0xabcd;
+	*req = (struct cxlmi_cci_msg) { 
+		.category = CXL_MCTP_CATEGORY_REQ,
+		.tag = mctp->tag++,
+		.command = MEMORY_DEVICE,
+		.command_set = IDENTIFY,
+		.vendor_ext_status = 0xabcd,
+	};
+	
+	/* if (ep->transport_data) { */
+	/* 	struct cxlmi_transport_mctp *mctp = ep->transport_data; */
 
-		if (req_pl_sz) {
-			printf("----> arm cci request setting pl_length\n");
-			req->pl_length[0] = req_pl_sz & 0xff;
-			req->pl_length[1] = (req_pl_sz >> 8) & 0xff;
-			req->pl_length[2] = (req_pl_sz >> 16) & 0xff;
-		}
-	}
+	/* 	req->category = CXL_MCTP_CATEGORY_REQ; */
+	/* 	req->tag = mctp->tag++; */
+	/* 	req->vendor_ext_status = 0xabcd; */
+
+	/* 	if (req_pl_sz) { */
+	/* 		printf("----> arm cci request setting pl_length\n"); */
+	/* 		req->pl_length[0] = req_pl_sz & 0xff; */
+	/* 		req->pl_length[1] = (req_pl_sz >> 8) & 0xff; */
+	/* 		req->pl_length[2] = (req_pl_sz >> 16) & 0xff; */
+	/* 	} */
+	/* } */
 
 	/* common to ioctl */
-	req->command_set = cmdset;
-	req->command = cmd;
+	/* req->command_set = cmdset; */
+	/* req->command = cmd; */
 
 	return 0;
 }
@@ -780,17 +790,17 @@ CXLMI_EXPORT int cxlmi_cmd_memdev_identify(struct cxlmi_endpoint *ep,
 	ssize_t rsp_sz;
 	struct cxlmi_transport_mctp *mctp = ep->transport_data;
 
-	if (!mctp)
+	/* if (!mctp) */
 		arm_cci_request(ep, &req, 0, IDENTIFY, MEMORY_DEVICE);
-	else {
-		req = (struct cxlmi_cci_msg) {
-			.category = CXL_MCTP_CATEGORY_REQ,
-			.tag = mctp->tag++,
-			.command = MEMORY_DEVICE,
-			.command_set = IDENTIFY,
-			.vendor_ext_status = 0xabcd,
-		};
-	}
+	/* else { */
+	/* 	req = (struct cxlmi_cci_msg) { */
+	/* 		.category = CXL_MCTP_CATEGORY_REQ, */
+	/* 		.tag = mctp->tag++, */
+	/* 		.command = MEMORY_DEVICE, */
+	/* 		.command_set = IDENTIFY, */
+	/* 		.vendor_ext_status = 0xabcd, */
+	/* 	}; */
+	/* } */
 	
 	rsp_sz = sizeof(*rsp) + sizeof(*rsp_pl);
 
